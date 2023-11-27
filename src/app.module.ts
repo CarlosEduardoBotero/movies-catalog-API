3699610fa58cf3eedefb32dfeadd657cb855de8c
Carlos Botero <boterocev@gmail.com>
2023-11-26 21:36:59 -0300

    added users/auth modules, controllers, and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users/users.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { UserEntity } from './users/entities/users.entity';
 
 @Module({
   imports: [
+    AuthModule,
+    UsersModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -17,13 +26,15 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [],
+        entities: [UserEntity],
         synchronize: configService.get('NODE_ENV') === 'development',
       }),
     }),
   ],
-  controllers: [],
+  controllers: [UsersController, AuthController],
   providers: [
+    AuthService,
+    UsersService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
