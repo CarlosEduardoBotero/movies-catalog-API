1b901820bab4d2699e82b18d8efadcff5ea34bd6
Carlos Botero <boterocev@gmail.com>
2023-11-27 00:32:57 -0300

    added imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,24 @@ import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesModule } from './movies/movies.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MoviesService } from './movies/movies.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { UserEntity } from './users/entities/users.entity';
 import { MovieEntity } from './movies/movies.entity';
+import { MovieRepository } from './movies/movies.repository';
 import { CacheService } from 'src/lib/cache/cache.service';
 
+console.log('pruba: ', __dirname + '/../**/*.entity{.ts}');
 @Module({
   imports: [
     AuthModule,
     UsersModule,
+    MoviesModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -29,10 +35,12 @@ import { CacheService } from 'src/lib/cache/cache.service';
       }),
     }),
   ],
-  controllers: [UsersController, AuthController],
+  controllers: [UsersController, AuthController, MoviesController],
   providers: [
     AuthService,
     UsersService,
+    MoviesService,
+    MovieRepository,
     CacheService,
     {
       provide: APP_GUARD,
